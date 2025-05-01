d74e40e972b698e651ebdb77024d615eebe31bad
Derek Kirschbaum <dgkirschbaum@ucsb.edu>
2025-04-30 18:17:46 -0700

    dk - fixed formatting for menu items fixtures

--- frontend/src/fixtures/UCSBDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/UCSBDiningCommonsMenuItemFixtures.js
@@ -1,30 +1,30 @@
-const menuItemsFixtures = {
-    oneMenuItem: {
-        "id": 4,
-        "diningCommonsCode": "Portola",
-        "name": "Chicken",
-        "station": "Entrees"
-      },
-    threeMenuItems: [
-        {
-          "id": 4,
-          "diningCommonsCode": "Portola",
-          "name": "Chicken",
-          "station": "Entrees"
-        },
-        {
-          "id": 5,
-          "diningCommonsCode": "Carrillo",
-          "name": "Cobb Salad",
-          "station": "Salad"
-        },
-        {
-          "id": 6,
-          "diningCommonsCode": "De La Guerra",
-          "name": "Cheeseburger",
-          "station": "Entrees"
-        }
-      ],
-  };
-  
-  export { ucsbDatesFixtures };
+const ucsbMenuItemsFixtures = {
+  oneMenuItem: {
+    id: 4,
+    diningCommonsCode: "Portola",
+    name: "Chicken",
+    station: "Entrees",
+  },
+  threeMenuItems: [
+    {
+      id: 4,
+      diningCommonsCode: "Portola",
+      name: "Chicken",
+      station: "Entrees",
+    },
+    {
+      id: 5,
+      diningCommonsCode: "Carrillo",
+      name: "Cobb Salad",
+      station: "Salad",
+    },
+    {
+      id: 6,
+      diningCommonsCode: "De La Guerra",
+      name: "Cheeseburger",
+      station: "Entrees",
+    },
+  ],
+};
+
+export { ucsbMenuItemsFixtures };
