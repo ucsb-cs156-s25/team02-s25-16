e7633785c3fa911a96011f211c4788d165850671
Derek Kirschbaum <dgkirschbaum@ucsb.edu>
2025-05-01 01:30:34 -0700

    dk - addressed mutation gaps for menu item form

--- frontend/src/main/components/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemForm.js
+++ frontend/src/main/components/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemForm.js
@@ -95,9 +95,7 @@ function UCSBDiningCommonsMenuItemForm({
         </Form.Control.Feedback>
       </Form.Group>
 
-      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
-        {buttonLabel}
-      </Button>
+      <Button type="submit">{buttonLabel}</Button>
       <Button
         variant="Secondary"
         onClick={() => navigate(-1)}
