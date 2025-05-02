556929e30c69a42611437d6b1653af273525ef56
Jeffrey Lin <jeffreylin@ucsb.edu>
2025-05-01 18:04:54 -0700

    fixed format

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,43 +1,42 @@
 const helpRequestFixtures = {
-    oneHelpRequest: {
+  oneHelpRequest: {
+    id: 1,
+    requesterEmail: "cgaucho@ucsb.edu",
+    teamId: "team-1",
+    tableOrBreakoutRoom: "16",
+    requestTime: "2025-05-05T01:01:05",
+    explanation: "the screen is broken",
+    solved: false,
+  },
+  threeHelpRequests: [
+    {
       id: 1,
-      requesterEmail: "cgaucho@ucsb.edu",
+      requesterEmail: "bob@ucsb.edu",
       teamId: "team-1",
       tableOrBreakoutRoom: "16",
       requestTime: "2025-05-05T01:01:05",
       explanation: "the screen is broken",
-      solved: false
+      solved: false,
     },
-    threeHelpRequests: [
-      {
-        id: 1,
-        requesterEmail: "bob@ucsb.edu",
-        teamId: "team-1",
-        tableOrBreakoutRoom: "16",
-        requestTime: "2025-05-05T01:01:05",
-        explanation: "the screen is broken",
-        solved: false
-      },
-      {
-        id: 2,
-        requesterEmail: "johnjohn@ucsb.edu",
-        teamId: "team-1",
-        tableOrBreakoutRoom: "16",
-        requestTime: "2025-05-05T01:01:05",
-        explanation: "the explanation is broken",
-        solved: false
-      },
-      {
-        id: 3,
-        requesterEmail: "chris@ucsb.edu",
-        teamId: "team-1",
-        tableOrBreakoutRoom: "16",
-        requestTime: "2025-05-05T01:01:05",
-        explanation: "the table is broken",
-        solved: false
-      },
-    ],
-  };
-  
-  export { helpRequestFixtures };
-  
+    {
+      id: 2,
+      requesterEmail: "johnjohn@ucsb.edu",
+      teamId: "team-1",
+      tableOrBreakoutRoom: "16",
+      requestTime: "2025-05-05T01:01:05",
+      explanation: "the explanation is broken",
+      solved: false,
+    },
+    {
+      id: 3,
+      requesterEmail: "chris@ucsb.edu",
+      teamId: "team-1",
+      tableOrBreakoutRoom: "16",
+      requestTime: "2025-05-05T01:01:05",
+      explanation: "the table is broken",
+      solved: false,
+    },
+  ],
+};
+
+export { helpRequestFixtures };
