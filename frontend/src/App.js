8452ac892d61a1da4da229f823d1bdba340131af
Jeffrey Lin <45890932+jeffreylin26@users.noreply.github.com>
2025-05-02 23:05:51 -0700

    Revert "WL - Copy Placeholders for RecRequest"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,10 +19,6 @@ import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
 import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
 
-import RecommendationRequestIndexPage from "main/pages/RecommendationRequest/RecommendationRequestIndexPage";
-import RecommendationRequestCreatePage from "main/pages/RecommendationRequest/RecommendationRequestCreatePage";
-import RecommendationRequestEditPage from "main/pages/RecommendationRequest/RecommendationRequestEditPage";
-
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -85,8 +81,8 @@ function App() {
           <>
             <Route
               exact
-              path="/recommendationrequest"
-              element={<RecommendationRequestIndexPage />}
+              path="/ucsbdiningcommonsmenuitem"
+              element={<UCSBDiningCommonsMenuItemIndexPage />}
             />
           </>
         )}
@@ -94,13 +90,13 @@ function App() {
           <>
             <Route
               exact
-              path="/recommendationrequest/edit/:id"
-              element={<RecommendationRequestEditPage />}
+              path="/ucsbdiningcommonsmenuitem/edit/:id"
+              element={<UCSBDiningCommonsMenuItemEditPage />}
             />
             <Route
               exact
-              path="/recommendationrequest/create"
-              element={<RecommendationRequestCreatePage />}
+              path="/ucsbdiningcommonsmenuitem/create"
+              element={<UCSBDiningCommonsMenuItemCreatePage />}
             />
           </>
         )}
