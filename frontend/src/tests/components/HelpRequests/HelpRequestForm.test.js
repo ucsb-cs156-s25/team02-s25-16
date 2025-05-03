833065accd9e3a6bd2c03adfd8cf6717f13be240
Jeffrey Lin <jeffreylin@ucsb.edu>
2025-05-03 00:18:33 -0700

    fix import

--- frontend/src/tests/components/HelpRequests/HelpRequestForm.test.js
+++ frontend/src/tests/components/HelpRequests/HelpRequestForm.test.js
@@ -2,7 +2,7 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import HelpRequestForm from "main/components/HelpRequests/HelpRequestForm";
-import helpRequestFixtures from "fixtures/helpRequestFixtures";
+import { helpRequestFixtures } from "fixtures/helpRequestFixtures";
 
 import { QueryClient, QueryClientProvider } from "react-query";
 
