f2de76302b1f7040c319a7f0526296d441a99050
applechristian <christianjlee@ucsb.edu>
2025-05-08 23:19:30 -0700

    cl - fixed utils

--- frontend/src/tests/utils/menuItemReviewUtils.test.js
+++ frontend/src/tests/utils/menuItemReviewUtils.test.js
@@ -1,7 +1,7 @@
 import {
   onDeleteSuccess,
   cellToAxiosParamsDelete,
-} from "main/utils/MenuItemReviewUtils";
+} from "main/utils/menuItemReviewUtils";
 import mockConsole from "jest-mock-console";
 
 const mockToast = jest.fn();
