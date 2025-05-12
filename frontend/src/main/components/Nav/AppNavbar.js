7b2b1397e726ed12e663d4765c1126020ae45c55
Suhrit Padakanti <suhrit@ucsb.edu>
2025-05-12 16:39:53 -0700

    code

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -17,6 +17,7 @@ export default function AppNavbar({
         currentUrl.startsWith("http://127.0.0.1:3000")) && (
         <AppNavbarLocalhost url={currentUrl} />
       )}
+
       <Navbar
         expand="xl"
         variant="dark"
@@ -31,6 +32,7 @@ export default function AppNavbar({
 
           <Navbar.Toggle />
 
+          {/* Left-side utility links */}
           <Nav className="me-auto">
             {systemInfo?.springH2ConsoleEnabled && (
               <Nav.Link href="/h2-console">H2Console</Nav.Link>
@@ -41,6 +43,7 @@ export default function AppNavbar({
           </Nav>
 
           <Navbar.Collapse className="justify-content-between">
+            {/* Main navigation */}
             <Nav className="mr-auto">
               {hasRole(currentUser, "ROLE_ADMIN") && (
                 <NavDropdown
@@ -51,61 +54,37 @@ export default function AppNavbar({
                   <NavDropdown.Item href="/admin/users">Users</NavDropdown.Item>
                 </NavDropdown>
               )}
-              {currentUser && currentUser.loggedIn && (
+
+              {currentUser?.loggedIn && (
                 <>
-                  <Nav.Link as={Link} to="/restaurants">
-                    Restaurants
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/ucsbdates">
-                    UCSB Dates
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/ucsborganization">
-                    UCSB Organization
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/diningcommonsmenuitem">
-                    Dining Commons Menu Item
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/recommendationrequests">
-                    Recommendation Requests
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">
-                    UCSB Dining Commons Menu Items
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/helprequests">
-                    Help Requests
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/placeholder">
-                    Placeholder
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/articles">
-                    Articles
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/recommendationRequest">
-                    Recommendation Request
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/menuitemreview">
-                    MenuItemReview
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/helprequest">
-                    Help Request
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/menuitemreviews">
-                    Menu Item Reviews
-                  </Nav.Link>
+                  <Nav.Link as={Link} to="/articles">Articles</Nav.Link>
+                  <Nav.Link as={Link} to="/diningcommonsmenuitem">Dining Commons Menu Item</Nav.Link>
+                  <Nav.Link as={Link} to="/helprequest">Help Request</Nav.Link>
+                  <Nav.Link as={Link} to="/helprequests">Help Requests</Nav.Link>
+                  <Nav.Link as={Link} to="/menuitemreview">MenuItemReview</Nav.Link>
+                  <Nav.Link as={Link} to="/menuitemreviews">Menu Item Reviews</Nav.Link>
+                  <Nav.Link as={Link} to="/placeholder">Placeholder</Nav.Link>
+                  <Nav.Link as={Link} to="/recommendationRequest">Recommendation Request</Nav.Link>
+                  <Nav.Link as={Link} to="/recommendationrequests">Recommendation Requests</Nav.Link>
+                  <Nav.Link as={Link} to="/restaurants">Restaurants</Nav.Link>
+                  <Nav.Link as={Link} to="/ucsbdates">UCSB Dates</Nav.Link>
+                  <Nav.Link as={Link} to="/ucsborganization">UCSB Organization</Nav.Link>
+                  <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">UCSB Dining Commons Menu Items</Nav.Link>
                 </>
               )}
             </Nav>
 
+            {/* Right-side auth */}
             <Nav className="ml-auto">
-              {currentUser && currentUser.loggedIn ? (
+              {currentUser?.loggedIn ? (
                 <>
                   <Navbar.Text className="me-3" as={Link} to="/profile">
-                    Welcome, {currentUser.root.user.email}
+                    Welcome,&nbsp;{currentUser.root.user.email}
                   </Navbar.Text>
-                  <Button onClick={doLogout}>Log Out</Button>
+                  <Button onClick={doLogout}>Log&nbsp;Out</Button>
                 </>
               ) : (
-                <Button href={oauthLogin}>Log In</Button>
+                <Button href={oauthLogin}>Log&nbsp;In</Button>
               )}
             </Nav>
           </Navbar.Collapse>
