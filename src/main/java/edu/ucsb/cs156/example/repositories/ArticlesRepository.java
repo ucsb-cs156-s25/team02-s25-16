363c6ddfb253bd1c19af2f8a08374a5fbfec400f
DerekKirschbaum <109634693+DerekKirschbaum@users.noreply.github.com>
2025-05-11 19:37:54 -0700

    Revert "dependencies"

--- src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.ucsb.cs156.example.repositories;
-
-import edu.ucsb.cs156.example.entities.Article;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticlesRepository extends CrudRepository<Article, Long> {}
