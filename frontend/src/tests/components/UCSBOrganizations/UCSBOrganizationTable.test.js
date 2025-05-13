afddf1f2604512d801fd12a0b03827974f178de3
John Hagedorn <johnhagedorn@ucsb.edu>
2025-05-12 21:48:36 -0700

    hi

--- frontend/src/tests/components/UCSBOrganizations/UCSBOrganizationTable.test.js
+++ frontend/src/tests/components/UCSBOrganizations/UCSBOrganizationTable.test.js
@@ -286,6 +286,6 @@ describe("UCSBOrganizationTable tests", () => {
 
     // assert - check that the delete endpoint was called
     await waitFor(() => expect(axiosMock.history.delete.length).toBe(1));
-    expect(axiosMock.history.delete[0].params).toEqual({ orgCode: "SKY" });
+    expect(axiosMock.history.delete[0].params).toEqual({ code: "SKY" });
   });
 });
